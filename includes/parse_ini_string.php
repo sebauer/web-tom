a1bee2839aaeb1c6759ca1087a697d1876de328c
gjlnetwork <gjlnetwork@a873545e-b3ea-feec-3399-bd67e05bc737>
2010-09-14 20:52:30 +0000

    Next try..

--- includes/parse_ini_string.php
+++ includes/parse_ini_string.php
@@ -2,30 +2,29 @@
 # Define parse_ini_string if it doesn't exist.
 # Does accept lines starting with ; as comments
 # Does not accept comments after values
-if( !function_exists('parse_ini_string') ){
-
-	function parse_ini_string( $string ) {
-		$array = Array();
-
-		$lines = explode("\n", $string );
-
-		foreach( $lines as $line ) {
-			$statement = preg_match(
-"/^(?!;)(?P<key>[\w+\.\-]+?)\s*=\s*(?P<value>.+?)\s*$/", $line, $match );
-
-			if( $statement ) {
-				$key    = $match[ 'key' ];
-				$value    = $match[ 'value' ];
-
-				# Remove quote
-				if( preg_match( "/^\".*\"$/", $value ) || preg_match( "/^'.*'$/", $value ) ) {
-					$value = mb_substr( $value, 1, mb_strlen( $value ) - 2 );
-				}
-
-				$array[ $key ] = $value;
-			}
-		}
-		return $array;
-	}
+if(!function_exists('parse_ini_string')){
+    function parse_ini_string($str, $ProcessSections=false){
+        $lines  = explode("\n", $str);
+        $return = Array();
+        $inSect = false;
+        foreach($lines as $line){
+            $line = trim($line);
+            if(!$line || $line[0] == "#" || $line[0] == ";")
+                continue;
+            if($line[0] == "[" && $endIdx = strpos($line, "]")){
+                $inSect = substr($line, 1, $endIdx-1);
+                continue;
+            }
+            if(!strpos($line, '=')) // (We don't use "=== false" because value 0 is not valid as well)
+                continue;
+            
+            $tmp = explode("=", $line, 2);
+            if($ProcessSections && $inSect)
+                $return[$inSect][trim($tmp[0])] = ltrim($tmp[1]);
+            else
+                $return[trim($tmp[0])] = ltrim($tmp[1]);
+        }
+        return $return;
+    }
 }
 ?>

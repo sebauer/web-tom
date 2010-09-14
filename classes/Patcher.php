c940a97a891d4d12fec846d26c3770a09e76c2b0
gjlnetwork <gjlnetwork@a873545e-b3ea-feec-3399-bd67e05bc737>
2010-09-14 00:21:19 +0000

    Added comments

--- classes/Patcher.php
+++ classes/Patcher.php
@@ -5,53 +5,86 @@ class Patcher {
     private $_patchedFile;
     private $_modifications = array( );
 
+    /**
+     * @param Modification $mod
+     * @return void
+     */
     public function addModification(Modification $mod){
         array_push($this->_modifications, $mod);
     }
 
+    /**
+     * @param string $sourceFilePath
+     * @param string $originalFilePath
+     * @return void
+     */
     public function createTunedFile($sourceFilePath, $originalFilePath){
+    	
+    	// Check if the original file matches the proper one
     	if(md5_file($originalFilePath)!='2e63a949d99dc19f62c36b43cb28d94e'){
     		throw new Exception('MD5 Checksum of original file incorrect!');
     	}
+    	
+    	// Open the original file read only
         $this->_originalFile = fopen($originalFilePath, 'r');
 
+        // Create a temporary output file and open it in binary write mode
+        // We'll create a copy of the source file which will then be modified
         $targetFileName = 'tmp'.md5($sourceFilePath).date('YmdHi').'.bin';
-
         copy($sourceFilePath, $targetFileName);
         $this->_patchedFile = fopen($targetFileName, 'r+b');
 
+        // Check if there are any modifications to be done
         if(count($this->_modifications) == 0){
             throw new Exception('No modifications given');
         }
 
+        // Now apply all modifications to the copy of our source file
         foreach($this->_modifications as $mod){
             $this->applyModification($mod);
         }
 
+        // And close..
         fclose($this->_originalFile);
         fclose($this->_patchedFile);
     }
 
+    /**
+     * @param Modification $mod
+     * @return void
+     */
     private function applyModification(Modification $mod){
+    	
+    	// First get the map for this modification
         $map = $mod->getMap();
+        // Get the selected setting
         $setting = $mod->getSetting();
 
+        // Get all ranges and iterate through them
         $ranges = $map->getRanges();
-
         foreach($ranges as $range){
+        	
+        	// Define start and end of the range
             $rangeStart = hexdec($range->getStart());
             $rangeEnd = hexdec($range->getEnd());
 
+            // Iterate through every single position within the range
             for($offset = $rangeStart; $offset <= $rangeEnd; $offset++){
 
+            	// Hex represantation of our decimal offset
                 $hexOffset = dechex($offset);
+                
+                // Seek to offset position
                 fseek($this->_patchedFile, $offset);
 
+                // Check if the setting contains a value for this position
                 if($setting->hasValueAtOffset($hexOffset)){
                 	$value = $setting->getValue($hexOffset);
                     fwrite($this->_patchedFile, pack('H*', $setting->getValue($hexOffset)->getValue()));
                     continue;
                 }
+                // Setting does not contain a value for this position, so
+                // we'll write the value of the original file from this position
                 fseek($this->_originalFile, $offset);
                 fwrite($this->_patchedFile, fread($this->_originalFile, 1));
             }
--- classes/Setting.php
+++ classes/Setting.php
@@ -1,36 +1,64 @@
 <?php
+
 class Setting {
     private $_key;
     private $_description;
     private $_listEntry;
     private $_values = array();
 
+    /**
+     * @param string $key
+     * @param string $description
+     * @param string $listEntry
+     * @return void
+     */
     public function __construct($key, $description, $listEntry){
         $this->_key = $key;
         $this->_listEntry = $listEntry;
         $this->_description = $description;
     }
 
+    /**
+     * @param Value $value
+     * @return void
+     */
     public function addValue(Value $value){
         $this->_values[$value->getOffset()] = $value;
     }
 
+    /**
+     * @param string $offset
+     * @return bool
+     */
     public function hasValueAtOffset($offset){
         return array_key_exists(strtoupper($offset), $this->_values);
     }
 
+    /**
+     * @param string $offset
+     * @return Value
+     */
     public function getValue($offset){
         return $this->_values[strtoupper($offset)];
     }
 
+    /**
+     * @return string
+     */
     public function getKey(){
         return $this->_key;
     }
     
+    /**
+     * @return string
+     */
     public function getDescription(){
     	return $this->_description;
     }
     
+    /**
+     * @return string
+     */
     public function getListEntry(){
     	return $this->_listEntry;
     }

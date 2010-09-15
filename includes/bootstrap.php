7bd10db1cf567985c56d4c75f541aeec4b6e2147
gjlnetwork <gjlnetwork@a873545e-b3ea-feec-3399-bd67e05bc737>
2010-09-15 17:45:07 +0000

    Added revision info

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * NEVER CHANGE ANYTHING IN THIS CONFIGURATION!
- * 
+ *
  * DO **NOT** TOUCH ANY OF THE DEFINES!
- * 
+ *
  */
 require_once('autoload.php');
 require_once('config.php');
@@ -25,3 +25,20 @@ define('_RANGESCOUNT', 'RangesCount');
 
 define('_SCOTTY_OFFSET', '1C3A8');
 define('_SCOTTY_STRING', 'CA0079006100CA009401A100CA00');
+
+if (!defined('_REVISION')) {
+	if (file_exists('.svn' . DIRECTORY_SEPARATOR. 'entries')) {
+		$svn = file('.svn' . DIRECTORY_SEPARATOR . 'entries');
+		if (is_numeric(trim($svn[3]))) {
+			$version = $svn[3];
+		} else { // pre 1.4 svn used xml for this file
+			$version = explode('"', $svn[4]);
+			$version = $version[1];
+		}
+		define ('_REVISION', trim($version));
+		unset ($svn);
+		unset ($version);
+	} else {
+		define ('_REVISION', 0); // default if no svn data avilable
+	}
+}

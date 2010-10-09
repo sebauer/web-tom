954bb6170f98bfae054aab6b53ddd0505c36e4d3
gjlnetwork <gjlnetwork@a873545e-b3ea-feec-3399-bd67e05bc737>
2010-10-09 12:11:46 +0000

    Updated config

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,7 @@
 
 define('_ORIGINALUPLOAD', 'upload/original/');
 define('_SOURCEUPLOAD', 'upload/source/');
+define('_EEPROMUPLOAD', 'upload/eeprom/');
 define('_SCOTTYCHECK', true);
 
 define('_DEBUG', false);
